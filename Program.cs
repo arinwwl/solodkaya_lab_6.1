214de06e7f327844263f57d9aea8bbcb11f7134f
arinwwl <ari.noo@mail.ru>
2023-12-22 15:00:22 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+Console.WriteLine("Введите систему исчисления: ");
+int p = int.Parse(Console.ReadLine());
+
+int result = ConvertToDecimal(number, p);
+Console.WriteLine(result);
+
+
+int ConvertToDecimal(string number, int p)
+{
+    int decimalNumber = 0;
+    int position = 0;
+
+    for (int i = number.Length - 1; i >= 0; i--)
+    {
+        int digit = int.Parse(number[i].ToString());
+        decimalNumber += digit * (int)Math.Pow(p, position);
+        position++;
+    }
+    return decimalNumber;
+}
+
